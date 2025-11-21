a885c65aeb5ac82024f41ec7cb047ed5032c333f
thenam-dev <ngthenam2801@gmail.com>
2025-11-21 10:50:41 +0700

    array type

---
+++ ts-studying/chapter1/lesson10/lesson10.js
@@ -0,0 +1,1 @@
+let myarr = ["nam", 13, true];
---
+++ ts-studying/chapter1/lesson10/lesson10.ts
@@ -0,0 +1,6 @@
+let test10: (string | number)[] = ["thenam", "alo"];
+// Luu tru mix type
+test10.push("them vao cuoi mang");
+test10.push(21);
+
+console.log(">>>check array:", test10);
