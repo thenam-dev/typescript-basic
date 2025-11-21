250871c6497d1b767d5b17d2f6e33f4b105fde58
thenam-dev <ngthenam2801@gmail.com>
2025-11-21 11:00:28 +0700

    tuple type

---
+++ ts-studying/chapter1/lesson11/lesson11.ts
@@ -0,0 +1,12 @@
+let skills: (string | number)[] = ["thenam", 10];
+skills.push("coding");
+skills.push(2004);
+
+console.log(skills);
+
+//tuple: dataType/size/order
+let skills2: [string, number] = ["nam", 28];
+
+let skills3: [boolean, string, number?]; //optional elements
+skills3 = [true, "alololo"];
+console.log(skills3);
