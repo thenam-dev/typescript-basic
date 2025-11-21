988b99b278f3f4efd0ecf4d28cd94a87912dbce0
thenam-dev <ngthenam2801@gmail.com>
2025-11-21 09:40:27 +0700

    annotation with TS

---
+++ ts-studying/chapter1/lesson4/lesson4.js
@@ -0,0 +1,10 @@
+let count2 = 1; //ok
+
+count1 = "name"; //error
+
+console.log(">>>check count2:", count2);
+
+let names = ["nam", "thenam", "typescript"]; // array bao gom string
+names.push(2025);
+
+console.log(names);
---
+++ ts-studying/chapter1/lesson4/lesson4.ts
@@ -0,0 +1,11 @@
+let count: number; //ok -> number: using annotation
+
+count = 10; //error
+
+console.log(">>>check count 2:", count);
+
+let names2: string[] = ["nam", "thenam", "typescript"];
+//array chi bao gom string
+names2.push("2025");
+
+console.log(">>>check name 2:", names2);
