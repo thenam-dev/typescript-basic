83108d19fee32c99776537c2346daefba2f62aeb
thenam-dev <ngthenam2801@gmail.com>
2025-11-21 09:52:57 +0700

    type inference

---
+++ ts-studying/chapter1/lesson5/lesson5.ts
@@ -0,0 +1,12 @@
+let count5: string = "the nam"; // tu dong gan type = string
+count5 = "nam";
+
+console.log(">>>check count 5:", count5);
+
+//Type inference: TS se doan kieu du lieu vao thuat toan cua no
+//Type annotation: dinh nghia ep buoc chinh xac kieu du lieu
+
+let test = ["typescript", "nam", "thenam"];
+test.push("nnn");
+
+//string =! String; number =! Number -> ko dung viet hoa
