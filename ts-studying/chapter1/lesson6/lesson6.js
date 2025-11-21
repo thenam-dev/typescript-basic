4e541e942882d830cad602d9e55db85e347fe471
thenam-dev <ngthenam2801@gmail.com>
2025-11-21 10:04:01 +0700

    number type

---
+++ ts-studying/chapter1/lesson6/lesson6.js
@@ -0,0 +1,2 @@
+let count6 = 69; //int long -> number
+let score = 9.6; //float double -> number
---
+++ ts-studying/chapter1/lesson6/lesson6.ts
@@ -0,0 +1,13 @@
+let test6: number;
+
+// test6 = "abc" -> error
+// Type 'string' is not assignable to type 'number'.
+
+test6 = 999999999999999999999999999999999;
+// error -> bigint
+
+const a1: number = 123;
+let a2: number;
+a2 = 50;
+console.log(test6);
+console.log(a1, a2);
