afe0fa1c30bc94f67e22370004adb9c1455e1f2c
thenam-dev <ngthenam2801@gmail.com>
2025-11-21 13:07:39 +0700

    enum type

---
+++ ts-studying/chapter1/lesson12/lesson12.ts
@@ -0,0 +1,13 @@
+enum API_STATUS {
+  PENDING = "PENDING",
+  FULFILLED = "FULFILLED",
+  REJECTED = "REJECT", //PROMISE
+}
+//constant
+//java
+
+let a11 = API_STATUS.PENDING;
+let a22 = API_STATUS.FULFILLED;
+
+//front-end: syntax
+console.log(">>> check a11 =", a11, " a22 =", a22);
