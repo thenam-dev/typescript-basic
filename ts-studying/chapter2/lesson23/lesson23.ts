7f5cc08a1f388eae30cd7e64c16479cae38a22cc
thenam-dev <ngthenam2801@gmail.com>
2025-11-22 17:45:31 +0700

    while loop

---
+++ ts-studying/chapter2/lesson23/lesson23.ts
@@ -0,0 +1,11 @@
+// while (condition) {
+//   dosomething;
+// }
+
+let counter = 0;
+
+while (counter < 5) {
+  console.log(counter);
+  if (counter % 2 === 1) break; // counter =1 -> thoat vong lap
+  counter++;
+}
