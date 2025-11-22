f39d042ca938690941ffb99a23c14d5f8e1ed3e4
thenam-dev <ngthenam2801@gmail.com>
2025-11-22 17:32:31 +0700

    switch case

---
+++ ts-studying/chapter2/lesson21/lesson21.ts
@@ -0,0 +1,13 @@
+const myAge: number = 20;
+switch (myAge) {
+  case 21: //waterfall
+  case 20:
+    //code block
+    console.log("Studying");
+    break;
+  case 30:
+    console.log("Married");
+    break;
+  default: // cac trg hop con lai
+    console.log("run default");
+}
