f96a4b747d2430a78c9dc9d7e83790a5f7064889
thenam-dev <ngthenam2801@gmail.com>
2025-11-22 17:49:59 +0700

    do-while loop

---
+++ ts-studying/chapter2/lesson24/lesson24.ts
@@ -0,0 +1,9 @@
+let counter2 = 0;
+
+do {
+  console.log("counter2 =", counter2);
+  if (counter2 % 2 === 1) break;
+  counter2++;
+} while (counter2 < 5);
+
+// do-while =! while: chay toi thieu 1 lan
